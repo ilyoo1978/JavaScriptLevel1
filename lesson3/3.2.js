4691f5cf854053576156a98efcc7aa7142114c07
ilyoo <ilyoo@yandex.ru>
2018-12-27 00:23:54 +0300

    задание 2

---
+++ lesson3/3.2.js
@@ -0,0 +1,10 @@
+function oddOrEven(start, end) { // выводит числа в промежутке start,end и сообщает чет это или нечет
+    let str;
+    do {
+        (start % 2 === 0)? str = ' - четное число': str = ' - нечетное число';
+        if ( start === 0){str = ' - это ноль'}
+        console.log(start + str);
+        start++;
+    }while (start <= end) ;
+}
+oddOrEven(0, 10);
